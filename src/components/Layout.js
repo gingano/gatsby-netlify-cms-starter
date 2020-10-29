f1d64b61365f10922a7c23eb38d38c751bf2dcd2
ginganoua <lesha.kowalenko@gmail.com>
2020-10-29 10:35:37 +0200

    initial commit

---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/main.scss'
+
+const TemplateWrapper = ({ children }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default TemplateWrapper
