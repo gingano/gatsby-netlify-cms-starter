845055daf07440311a2bcae8ca48ec764c3b66f4
ginganoua <lesha.kowalenko@gmail.com>
2020-11-05 13:53:30 +0200

    slider added

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,10 +1,13 @@
 import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
+import { SlidesControl, SlidesPreview } from './Slides'
 
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
+
+CMS.registerWidget('slides', SlidesControl, SlidesPreview)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
